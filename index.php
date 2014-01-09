1926c34ae4e631d2693232c6d04b1eb0b43af964
Zurc <cruzjurado@gmail.com>
2014-01-09 17:42:48 +0000

    alinear titulos index y about

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('header.php');
  
 	<section class="welcome">
 		<div class="row">
-			<div class="large-8 right columns">
+			<div class="large-8 right columns pad-left">
 				<h2>Design to fit every need</h2>
 			</div>
 		</div>
