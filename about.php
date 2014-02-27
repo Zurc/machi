6b60c39fa91a9a923a9863bcfd589ee954bab50e
Zurc <cruzjurado@gmail.com>
2014-02-27 13:14:12 +0000

    cambio titulos y parrafos

--- about.php
+++ about.php
@@ -17,14 +17,14 @@ include ('header.php');
 	
 	<article class="contact-me">
 		<div class="row">
-			<div class="large-6 columns bmargin-top bpadding-right">
+			<div class="large-6 columns bpadding-right">
 				<h4>About myself</h4>
 				<p class="about">After obtaining a BA degree in graphic design at Buenos Aires University in Argentina, I have worked for a dozen years perfecting the right mix of skills and processes to deliver great work for my clients.</p>
 				<p class="about">I have worked globally for retail; publishing and media companies, understanding their culture and helping them develop design concepts from branding, packaging to publishing and advertisement.</p>
 			</div>
-			<div class="large-6 columns bmargin-top bpadding-left">
+			<div class="large-6 columns bpadding-left">
 				<p class="extra">I believe that exceptional design is usable, simple, and beautiful. Creating great design requires a balance of raw talent, usability instincts, user empathy, keen business acumen, and a deep understanding of technical feasibility.</p> 
-				<p class="extra">I combine great ideas and business sense with the very best of graphic design.</p>
+				<p>I combine great ideas and business sense with the very best of graphic design.</p>
 
 				<!-- <h4>Places where I worked</h4>
 				<img src="img/map.png" alt="">
